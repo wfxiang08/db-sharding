53759bc526dc052386c1b0c164cc1a3da426da7f
fei.wang <fei.wang@ushow.media>
2017-11-11 19:12:20 +0800

    添加了Nullable的数据类型的处理

--- src/models/builder.go
+++ src/models/builder.go
@@ -1,5 +1,7 @@
 package models
 
+import "database/sql/driver"
+
 //
 // 1. 实现model(interface{})到SQL转换
 // 2. 实现binlog的args, where到SQL转换
@@ -11,4 +13,20 @@ type ModelBuilder interface {
 	InsertIgnore(model interface{}) *ShardingSQL
 	GetShardingIndex4Model(model interface{}) int
 	GetBatchInsertSegment() string
+}
+
+// Value is a value that drivers must be able to handle.
+// It is either nil or an instance of one of these types:
+//
+//   int64
+//   float64
+//   bool
+//   []byte
+//   string
+//   time.Time
+// WARNING: 参考: go-sql-driver/mysql中 interpolateParams的实现，其他类型的数据被统一处理为空了
+//
+func Nullable2Value(v driver.Valuer) driver.Value {
+	result, _ := v.Value()
+	return result
 }
